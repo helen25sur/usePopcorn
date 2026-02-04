abb4b7cca6f865befd7e93036d5585842b93500a
Olena Surilova <elen21surilova@gmail.com>
2026-02-04 20:45:50 +0100

    feat: create WatchedListItem & MovieListItem

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,10 @@
+import MovieListItem from "./MovieListItem";
+
 export default function MovieList({ movies }) {
   return (
     <ul className="list">
       {movies?.map((movie) => (
-        <li key={movie.imdbID}>
-          <img src={movie.Poster} alt={`${movie.Title} poster`} />
-          <h3>{movie.Title}</h3>
-          <div>
-            <p>
-              <span>🗓</span>
-              <span>{movie.Year}</span>
-            </p>
-          </div>
-        </li>
+        <MovieListItem key={movie.imdbID} movie={movie} />
       ))}
     </ul>
   )
