459a9a6c801c85196d94e405703d41a5b5f55e0b
Olena Surilova <elen21surilova@gmail.com>
2026-02-04 20:34:39 +0100

    feat: create Stats component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
 import Box from './Box';
 import MovieList from './MovieList';
+import Stats from './Stats';
 
 export default function Main({ average, movies, watched }) {
 
   const [isOpen1, setIsOpen1] = useState(true);
   const [isOpen2, setIsOpen2] = useState(true);
 
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
 
   return (
     <main className="main container flex justify-center gap-6 m-auto p-6 text-white">
@@ -22,27 +21,7 @@ export default function Main({ average, movies, watched }) {
       <Box setIsOpen={setIsOpen2} isOpen={isOpen2}>
         {isOpen2 && (
           <>
-            <div className="summary p-4 bg-neutral-600 rounded-md">
-              <h2 className='text-xl font-medium uppercase'>Movies you watched</h2>
-              <div className='flex items-center justify-between'>
-                <p>
-                  <span>#️⃣</span>
-                  <span>{watched.length} movies</span>
-                </p>
-                <p>
-                  <span>⭐️</span>
-                  <span>{avgImdbRating}</span>
-                </p>
-                <p>
-                  <span>🌟</span>
-                  <span>{avgUserRating}</span>
-                </p>
-                <p>
-                  <span>⏳</span>
-                  <span>{avgRuntime} min</span>
-                </p>
-              </div>
-            </div>
+            <Stats average={average} watched={watched} />
 
             <ul className="list">
               {watched.map((movie) => (
---
+++ src/components/Stats.jsx
@@ -0,0 +1,29 @@
+export default function Stats({ average, watched }) {
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return (
+    <div className="summary p-4 bg-neutral-600 rounded-md">
+      <h2 className='text-xl font-medium uppercase'>Movies you watched</h2>
+      <div className='flex items-center justify-between'>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+    </div>
+  )
+}
