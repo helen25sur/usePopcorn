73275f9206195204333beb180d6013d31b5ae376
Olena Surilova <elen21surilova@gmail.com>
2026-03-15 22:19:53 +0100

    fix: use async function

--- src/App.jsx
+++ src/App.jsx
@@ -61,15 +61,19 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("interstellar");
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search))
-  }, [])
+  useEffect(() => {
+    async function fetchMovies() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${query}`)
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+
+    fetchMovies();
+  }, [query])
 
   return (
     <div className="bg-neutral-800 w-full min-h-screen p-3">
